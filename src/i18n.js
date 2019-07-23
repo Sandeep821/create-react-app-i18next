df2f4d941f271952ca515b9201d798346bc21911
Soni <Sandeep.Soni@vwcredit.com>
2019-07-23 15:59:47 -0500

    legacy way ver 9

--- src/i18n.js
+++ src/i18n.js
@@ -3,20 +3,21 @@ import { initReactI18next } from 'react-i18next';
 
 import Backend from 'i18next-xhr-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
-// not like to use this?
-// have a look at the Quick start guide 
-// for passing in lng and translations on init
 
+import translationEN from './trans/en/translation';
+import translationDE from './trans/de/translation';
 
-import common_de from "./translations/de/common.json";
-import common_en from "./translations/en/common.json";
+// the translations
+const resourceContent = {
+  en: {
+    translation: translationEN
+  },
+  de: {
+    translation: translationDE
+  }
+};
 
 i18n
-  // load translation using xhr -> see /public/locales
-  // learn more: https://github.com/i18next/i18next-xhr-backend
-  .use(Backend)
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
   .use(LanguageDetector)
   // pass the i18n instance to react-i18next.
   .use(initReactI18next)
@@ -28,7 +29,9 @@ i18n
 
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
-    }
+    },
+    resources: resourceContent,
+    keySeparator: true
   });
 
 
